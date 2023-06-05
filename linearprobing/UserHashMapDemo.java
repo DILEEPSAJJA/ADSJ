ee1f940ff3f0bcbf77fc34ce7c63fd6cdb3ae794
DILEEPSAJJA <118289961+DILEEPSAJJA@users.noreply.github.com>
2023-06-05 11:48:04 +0530

    Add files via upload

---
+++ linearprobing/UserHashMapDemo.java
@@ -0,0 +1,46 @@
+package linearprobing;
+
+import java.util.*;
+
+public class UserHashMapDemo {
+	public static void main(String[] args) {
+		UserHashMap<Integer, String> hm = new UserHashMap<>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1)put\t2)get\t3)remove\t4)display\t5)exit\n");
+		while(true) {
+		System.out.println("Enter Your Choice: ");
+		int ch = sc.nextInt();
+		
+		switch (ch) {
+		case 1: System.out.println("Enter key, value : ");
+				Integer index = sc.nextInt();
+				String name = sc.next();
+				if(hm.put(index, name))
+					System.out.println(name + " is added.");
+				break;
+				
+		case 2: System.out.println("Enter key : ");	
+				index = sc.nextInt();
+				String ele = hm.get(index);
+				if(ele != null)
+					System.out.println("the value at index is : " + ele);
+				break;
+			
+		case 3: System.out.println("Enter key : ");
+				index = sc.nextInt();
+				ele = hm.remove(index);
+				if(ele != null)
+					System.out.println("the value at index removed is : " + ele);
+				break;
+			
+		case 4: hm.display();
+				break;
+			
+		case 5: sc.close();
+				System.exit(0);
+			
+		default: System.out.println("Enter valid input."); 
+		}
+	}
+	}
+}
